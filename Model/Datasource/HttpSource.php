91e2b05e44a0d61421f1deb0583ce39a991add7c
Aleksandr <skuridin-alex@ya.ru>
2014-03-31 18:29:21 +0300

    Need to configure log row for each Source

--- Model/Datasource/HttpSource.php
+++ Model/Datasource/HttpSource.php
@@ -181,6 +181,13 @@ abstract class HttpSource extends DataSource {
 	protected $_queryData = array();
 
 	/**
+	 * Row log
+	 *
+	 * @var array
+	 */
+	protected $log_row = array();
+
+	/**
 	 * Constructor
 	 *
 	 * @param array $config
@@ -322,19 +329,14 @@ abstract class HttpSource extends DataSource {
 	/**
 	 * Log given query.
 	 *
-	 * @param array $params Values binded to the query (prepared statements)
 	 * @return void
 	 */
 	public function logRequest() {
 		$this->_requestsCnt++;
-		$log = array(
-			'query' => strlen($this->query) > static::LOG_MAX_LENGTH ? substr($this->query, 0, static::LOG_MAX_LENGTH) . ' ' . static::LOG_TRUNCATED : $this->query,
-			'error' => $this->error,
-			'affected' => $this->affected,
-			'numRows' => $this->numRows,
-			'took' => $this->took,
-		);
-		$this->_requestsLog[] = $log;
+
+		$this->logPrepare();
+
+		$this->_requestsLog[] = $this->log_row;
 		$this->_requestsTime += $this->took;
 		if (count($this->_requestsLog) > $this->_requestsLogMax) {
 			array_shift($this->_requestsLog);
@@ -343,6 +345,20 @@ abstract class HttpSource extends DataSource {
 		if (!empty($this->error)) {
 			$this->log(get_class($this) . ': ' . $this->error . "\n" . $log['query'], LOG_ERR);
 		}
+	}
+
+	 /**
+	 * Log query prepare.
+	 *
+	 * @return void
+	 */
+	public function logPrepare() {
+		$this->log_row = array(
+			'query' => mb_strlen($this->query) > static::LOG_MAX_LENGTH ? mb_substr($this->query, 0, static::LOG_MAX_LENGTH) . ' ' . static::LOG_TRUNCATED : $this->query,
+			'affected' => $this->affected,
+			'numRows' => $this->numRows,
+			'took' => $this->took,
+		);
 	}
 
 	/**
